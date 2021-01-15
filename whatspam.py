2805879750fc71e0f7242b0311c8b5dcce6abe30
Florian Zwonarz <flozwonarz@hotmail.com>
2021-01-15 08:38:19 +0100

    New Features
    -Send Ping request
    -Wait until all messages been send

--- whatspam.py
+++ whatspam.py
@@ -3,24 +3,29 @@ import pickle
 import random
 import sys
 import string
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.common.exceptions import InvalidArgumentException, NoSuchElementException
+from selenium.common.exceptions import InvalidArgumentException, NoSuchElementException, InvalidSessionIdException
+from selenium.webdriver.support import expected_conditions as EC
+from urllib3.exceptions import MaxRetryError
+
 
 import requests
 
 # variable to store the name of the contact
-contact = "?asd"
+contact = "Testing"
 #variable to store message
-message = "Hi, bin heute fertig"
+message = "Hi, this is a automated Text message by https://github.com/Florian-Mahlberg/whatspam"
 timeForFor = 20
-answer = "Ich bin Tom"
+time_for_ending_servers = 60
+answer = "answer"
 url = "https://web.whatsapp.com"
 rightInfos = False
-
+exitProgramm = False
 
 
 #Wait for the QR Code
@@ -46,20 +51,25 @@ def check_contact_info():
     except IndexError:
         return False
 
-
-
 def set_contact():
-    return input("Please input contact's name: ")
+    loc = input("Please input contact's name: ")
+    if loc == "" or loc == " ":
+        return "Testing"
+    else:
+        return loc
 
 def set_message():
-    return input("Please input message to spam: ")
+    loc = input("Please input message to spam: ")
+    if loc == "" or loc == " ":
+        return "Hi, this is a automated Text message by https://github.com/Florian-Mahlberg/whatspam"
+    else:
+        return loc
 
 def set_times():
     try:
         return int(input("Please input time to spam: "))
     except ValueError:
         return 20
-
 
 #finding search bar
 def find_contact(contact):
@@ -126,11 +136,14 @@ except InvalidArgumentException:
     print('\n'+ '\n'+ "I wanted to start Chrome, but you started it already." + '\n'+ "Please close it, so I can create a fresh browser webpage!"+ '\n'+ '\n')
     sys.exit()
 
+response = os.system("ping -c 1 " + "google.com")
+if response == 0:
+    print ("google", 'is up!')
+else:
+    print ("google", 'is down!')
+
 
 try:
-
-
-
     while wait_for_code() == False:
         time.sleep(0.5)
 
@@ -153,32 +166,66 @@ try:
     #prints the automated message multiple times
     for count in range(timeForFor):
         send_message(f'{message} {count+1}/{timeForFor}')
-
-        all_children_by_xpath = driver.find_elements_by_xpath('.//*[@id="main"]/div[3]/div/div/div[3]')
-        print('len(all_children_by_xpath): ' + str(len(all_children_by_xpath)))
-
-
-    aria_label = btn.find_element_by_css_selector('span').get_attribute("aria-label")
-
-    while aria_label == Pending:
-        time.sleep(0.5)
+        btn = driver.find_element_by_xpath('//*[@id="main"]/div[3]/div/div/div[3]/div[last()]/div/div/div/div[2]/div/div')
         aria_label = btn.find_element_by_css_selector('span').get_attribute("aria-label")
 
-    time.sleep(10)
+    i = 0
+    while str(aria_label) == " Pending ":
+        print("Contacting WhatsApp Servers " + str(i/2) + "/" + str(time_for_ending_servers/2))
+        try:
+            aria_label = btn.find_element_by_css_selector('span').get_attribute("aria-label")
+        except MaxRetryError:
+            print("Timeout")
+            response = os.system("ping -c 1 " + "google.com")
 
-    driver.close()
+            if response == 0:
+                print("Can't reach your phone, please check, if your Phone has a stable connection!")
+            else:
+                print("Can't contact the WhatsApp Servers, please check, that your Phone and Computer has a stable connection.")
+
+            try:
+                driver.close()
+                driver.quit()
+            except MaxRetryError:
+                sys.exit()
+        i = i + 1
+        if i >= time_for_ending_servers:
+            driver.close()
+            driver.quit()
+            exitProgramm = False
+        time.sleep(0.5)
+
+    try:
+        driver.close()
+        exitProgramm = False
+    except ProtocolError:
+        time.sleep(0)
+    exitProgramm = False
 finally:
+    if exitProgramm == True:
+        try:
+            btn = driver.find_element_by_xpath('//*[@id="main"]/div[3]/div/div/div[3]/div[last()]/div/div/div/div[2]/div/div')
+            aria_label = btn.find_element_by_css_selector('span').get_attribute("aria-label")
 
-    print(" <- Thats a Ctrl+C ? I'll stop for you! Please don't press Ctrl+C until the programm is closed.")
-    print("Waiting for messages, that didn't want to go to the Server...")
+            print(" <- Thats a Ctrl+C ? I'll stop for you! Please don't press Ctrl+C until the programm is closed.")
+            print("Waiting for messages, that didn't want to go to the Server...")
 
-#    btn = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/div/div/div[3]/div[94]/div/div/div/div[2]/div/div/span')
-#    aria_label = btn.find_element_by_css_selector('span').get_attribute("aria-label")
-
-#    while aria_label == Pending:
-#        time.sleep(0.5)
-#        aria_label = btn.find_element_by_css_selector('span').get_attribute("aria-label")
+            i = 0
+            while str(aria_label) == "Pending":
+                print("Contacting WhatsApp Servers " + str(i/2) + "/" + str(time_for_ending_servers/2))
+                aria_label = btn.find_element_by_css_selector('span').get_attribute("aria-label")
+                i = i + 1
+                if i >= range(time_for_ending_servers):
+                    driver.close()
+                    driver.quit()
+                time.sleep(0.5)
+            time.sleep(1)
+            driver.close()
+        except InvalidSessionIdException:
+            time.sleep(0)
+        except NoSuchElementException:
+            time.sleep(0)
+        except MaxRetryError:
+            time.sleep(0)
 
     print("Bye, I hope you come back and don't forget me!")
-    time.sleep(0.5)
-    driver.close()
